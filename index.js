f3e6f8591544d5393deff8179f64604c9fbe2b0f
Aimaral <116299759+Aimaral95@users.noreply.github.com>
2025-12-12 18:39:57 -0600

    updated the CSS to look more like a Figma design

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
 import { menuArray } from "./data.js";
 
-const menuItem = document.getElementById("menu-item");
+const menuItem = document.getElementById("menu");
 
-function renderMenuItem(menu) {
-  let menuItems = "";
-
-  for (let item of menu) {
-    menuItems += `
+function renderMenuItem(menu){
+  menuItem.innerHTML = menu
+    .map(item => `
+      <div class="menu-item">
         <span class="emoji">${item.emoji}</span>
+
         <div class="description">
           <h1>${item.name}</h1>
           <p>${item.ingredients.join(", ")}</p>
           <h2>$${item.price}</h2>
         </div>
-        <button class="add-btn">+</button>
-    `;
-  }
 
-  menuItem.innerHTML = menuItems;
+        <button class="add-btn">+</button>
+      </div>
+    `)
+    .join("");
 }
 
 renderMenuItem(menuArray);
